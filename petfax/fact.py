bc2a05e0bece395c8f80fe1c2d1988caa2d3b3a1
martin0424 <h.newland424@gmail.com>
2023-05-03 13:21:21 -0400

    finalpush

---
+++ petfax/fact.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, render_template, request, redirect
+
+bp = Blueprint("fact", __name__, url_prefix="/facts", template_folder="templates")
+
+
+@bp.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        print(request.form)
+        return redirect("/facts")
+
+    return render_template("facts/index.html")
+
+
+@bp.route("/new")
+def fact():
+    return render_template("facts/new.html")
